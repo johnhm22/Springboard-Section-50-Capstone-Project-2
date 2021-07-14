f2d8f4609303c2fb1f4166b69fc3662f1a6b5aa7
John Morgan <johnhmorgan@outlook.com>
2021-07-14 19:36:00 +0100

    adding formik config for forms

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,47 +1,91 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import './LoginForm.css';
+import { useFormik } from 'formik';
 
+
+const validate = values => {
+    const errors = {};
+
+    if(!values.username) {
+        errors.username = "username required";
+    } 
+    if(!values.password) {
+        errors.password = "password required";
+    } 
+    return errors;
+    }
 
 const LoginForm = ({login}) => {
 
-    const initialState = {
-        username: "",
-        password: ""
-    }
-    const [formData, setFormData] = useState(initialState);
-
     const history = useHistory();
 
-const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormData(formdata => ({
-        ...formdata,
-        [name]: value
-    }))
-}
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const {username, password} = formData;
-    login(username, password);
-    setFormData(initialState);
-    history.push('/');
-}
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validate,
+        onSubmit: values => {
+            login(values.username, values.password);
+            history.push('/');
+        },
+    });
+
+    
+    // const initialState = {
+    //     username: "",
+    //     password: ""
+    // }
+    // const [formData, setFormData] = useState(initialState);
+
+
+// const handleChange = (e) => {
+//     const {name, value} = e.target;
+//     setFormData(formdata => ({
+//         ...formdata,
+//         [name]: value
+//     }))
+// }
+
+// const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const {username, password} = formData;
+//     login(username, password);
+//     setFormData(initialState);
+//     history.push('/');
+// }
 
 return (
     <div className="container col-lg-6">
         <div className="row justify-content-md-center">
         <div className="col-6">
-        <form method="get" onSubmit={handleSubmit} className="mt-3">
+        <form method="get" onSubmit={formik.handleSubmit} className="mt-3">
         <h3>Log In</h3>
             <div className="form-group">
             <label htmlFor="username"></label>
-            <input className="form-control" type="text" name="username" id="username" value={formData.username} onChange={handleChange} placeholder="Enter username..." />
+            <input 
+            className="form-control" 
+            type="text" name="username" 
+            id="username" 
+            value={formik.username}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Enter username..." />
+            {formik.errors.username ? <div style={{color:'red'}}>{formik.errors.username}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="password"></label>
-            <input className="form-control" type="text" name="password" id="password" value={formData.password} onChange={handleChange} placeholder="Enter password..." />
+            <input
+            className="form-control" 
+            type="text" name="password" 
+            id="password" 
+            value={formik.password} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Enter password..." />
+            {formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div> : null}
             </div>
             <button className="btn btn-primary btn-sm float-right">Log in</button>
         </form>
--- frontend/src/SignupForm.js
+++ frontend/src/SignupForm.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import './SignupForm.css';
+import { useFormik } from 'formik';
 
 
+const validate = values => {
+    const errors = {};
+
+    if(!values.username) {
+        errors.username = "username required";
+    } 
+    if(!values.password) {
+        errors.password = "password required";
+    } 
+    if(!values.firstname) {
+        errors.firstname = "first name required";
+    } 
+    if(!values.lastname) {
+        errors.lastname = "last name required";
+    } 
+    if(!values.email) {
+        errors.email = "email required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email format';
+    }
+    if(!values.firstProperty) {
+        errors.firstProperty = "at least one property required";
+    } 
+    return errors;
+    }
 
 const SignupForm = ({signup}) => {
 
     const history = useHistory();
 
-    const initialState = {
+    const formik = useFormik({
+        initialValues: {
         username: "",
         password: "",
         firstname: "",
@@ -17,73 +44,145 @@ const SignupForm = ({signup}) => {
         firstProperty: "",
         secondProperty: "",
         thirdProperty: ""
-    }
-    const [formData, setFormData] = useState(initialState);
+    },
+    validate,
+    onSubmit: values => {
+        signup(values.username, values.password, values.firstname, values.lastname, values.email, values.firstProperty, values.secondProperty, values.thirdProperty);
+        history.push('/');
+        }
+    });
 
-    const [isInvalid, setIsInvalid] = useState(true);
-    const [isTouched, setIsTouched] = useState(false);
+   
 
 
-const handleChange = (e) => {
-    setIsTouched(true);
-    const {name, value} = e.target;
-    if(value === '') {
-        setIsInvalid(true);
-    } else {
-        setIsInvalid(false);
-    }
-    setFormData(formdata => ({
-        ...formdata,
-        [name]: value
-    }))
-}
+// const handleChange = (e) => {
+//     setIsTouched(true);
+//     const {name, value} = e.target;
+//     if(value === '') {
+//         setIsInvalid(true);
+//     } else {
+//         setIsInvalid(false);
+//     }
+//     setFormData(formdata => ({
+//         ...formdata,
+//         [name]: value
+//     }))
+// }
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const {username, password, firstname, lastname, email, firstProperty, secondProperty, thirdProperty} = formData;
-    signup(username, password, firstname, lastname, email, firstProperty, secondProperty, thirdProperty);
-    setFormData(initialState);
-    history.push('/');
-}
+// const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const {username, password, firstname, lastname, email, firstProperty, secondProperty, thirdProperty} = formData;
+//     signup(username, password, firstname, lastname, email, firstProperty, secondProperty, thirdProperty);
+//     setFormData(initialState);
+//     history.push('/');
+// }
 
 return (
     <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
         <div className="row justify-content-md-center">
         <div className="col-10">
-        <form method="get" onSubmit={handleSubmit} className="mt-2">
+        <form method="get" onSubmit={formik.handleSubmit} className="mt-2">
         <h3>Sign Up</h3>
             <div className="form-group">
             <label htmlFor="username" className="float-left"></label>
-            <input className="form-control" type="text" name="username" id="username" value={formData.username} onChange={handleChange} placeholder="Enter username..." />
+            <input
+            className="form-control" 
+            type="text" 
+            name="username" 
+            id="username" 
+            value={formik.username} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Enter username..." />
+            {formik.errors.username ? <div style={{color:'red'}}>{formik.errors.username}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="password" className="float-left"></label>
-            <input className="form-control" type="text" name="password" id="password" value={formData.password} onChange={handleChange} placeholder="Enter password..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="password" 
+            id="password" 
+            value={formik.password}
+            onBlur={formik.handleBlur} 
+            onChange={formik.handleChange} 
+            placeholder="Enter password..." />
+            {formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="firstname" className="float-left"></label>
-            <input className="form-control" type="text" name="firstname" id="firstname" value={formData.firstname} onChange={handleChange} placeholder="Enter first name..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="firstname" 
+            id="firstname" 
+            value={formik.firstname} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} placeholder="Enter first name..." />
+             {formik.errors.firstname ? <div style={{color:'red'}}>{formik.errors.firstname}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="lastname" className="float-left"></label>
-            <input className="form-control" type="text" name="lastname" id="lastname" value={formData.lastname} onChange={handleChange} placeholder="Enter last name..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="lastname" 
+            id="lastname" 
+            value={formik.lastname} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Enter last name..." />
+            {formik.errors.lastname ? <div style={{color:'red'}}>{formik.errors.lastname}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="email" className="float-left"></label>
-            <input className="form-control" type="text" name="email" id="email" value={formData.email} onChange={handleChange} placeholder="Enter email..." />
-            {isInvalid && isTouched && <span style = {{color: 'red'}}>Email cannot be blank!</span>}
+            <input 
+            className="form-control" 
+            type="text" 
+            name="email" 
+            id="email" 
+            value={formik.email} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Enter email..." />
+            {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="firstProperty" className="float-left"></label>
-            <input className="form-control" type="text" name="firstProperty" id="firstProperty" value={formData.firstProperty} onChange={handleChange} placeholder="First property number..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="firstProperty" 
+            id="firstProperty" 
+            value={formik.firstProperty} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="First property number..." />
+             {formik.errors.firstProperty ? <div style={{color:'red'}}>{formik.errors.firstProperty}</div> : null}
             </div>
             <div className="form-group">
             <label htmlFor="secondProperty" className="float-left"></label>
-            <input className="form-control" type="text" name="secondProperty" id="secondProperty" value={formData.secondProperty} onChange={handleChange} placeholder="Second property number..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="secondProperty" 
+            id="secondProperty" 
+            value={formik.secondProperty} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Second property number..." />
             </div>
             <div className="form-group">
             <label htmlFor="thirdProperty" className="float-left"></label>
-            <input className="form-control" type="text" name="thirdProperty" id="thirdProperty" value={formData.thirdProperty} onChange={handleChange} placeholder="Third property number..." />
+            <input 
+            className="form-control" 
+            type="text" 
+            name="thirdProperty" 
+            id="thirdProperty" 
+            value={formik.thirdProperty} 
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange} 
+            placeholder="Third property number..." />
             </div>
             <button className="btn btn-primary btn-sm float-right">Sign up</button>
 
